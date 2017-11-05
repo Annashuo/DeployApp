dda50191b9f9a684775595fa2da769bad0c96a3c
Annashuo <shuoshuo_yi@163.com>
2017-11-05 20:59:58 +0000

    deploy success

--- webapps/settings.py
+++ webapps/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'hq%yw)lvpfw6ofu7vsufvm1e4mhd=8u6ay7iks^1o=t$o8do$%'
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'hq%yw)lvpfw6ofu7vsufvm1e4mhd=8u6ay7iks^1o=t$o8do$%'
+#SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.208.168.175']
 
 
 # Application definition
@@ -85,25 +85,20 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'grumblr',
+		'USER': 'root',
+		'PASSWORD': '123456',
+	}
+}
+#    DATABASES = {
+#        'default': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        }
+#    }
 
 
 # Password validation
@@ -146,14 +141,12 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_FROM_EMAIL = 'shuoh1@andrew.cmu.edu'
 SERVER_EMAIL = 'shuoh1@andrew.cmu.edu'
 EMAIL_HOST = 'smtp.andrew.cmu.edu'       #'smtp.andrew.cmu.edu'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shuoh1'    #andrew id
-EMAIL_HOST_PASSWORD = 'xxx'
+EMAIL_HOST_PASSWORD = 'Hua_nong_ying1120'
 EMAIL_USE_TLS = True
